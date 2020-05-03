b9b4fe1ece42be6349cdb8470db24902d6835673
LBC <youaadd@163.com>
2020-05-03 17:59:44 +0800

    想法改变，需要一个简单的辅助器。

--- dbtools/src/main/java/com/dglbc/dbtools/assistant/Select.java
+++ dbtools/src/main/java/com/dglbc/dbtools/assistant/Select.java
@@ -1,25 +1,66 @@
 package com.dglbc.dbtools.assistant;
 
 import com.dglbc.dbtools.Expression;
-import com.dglbc.dbtools.join.Join;
-import com.dglbc.dbtools.table.Column;
-import com.dglbc.dbtools.table.Table;
 import com.dglbc.dbtools.where.Where;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
+/*
+    退一万步来说，直接生成就好，不需要残留，不需要这么复杂的生成过程。
+    不是要面面俱到，而是要辅助写sql语句和看起来不这么乱。不会检查语法，不会智能的干什么。单纯就是指哪打哪。（理念清晰，之前的想法先放一放）
+ */
+@Accessors(chain = true)
+@Setter
+@Getter
+@NoArgsConstructor
 public class Select {
-    private Table table;
-    private Map<String,Expression> content;//查询的名称
-    private Expression special;
-    private Map<String,Where> conditions;
-    private Map<String,Join> joins;
-    private boolean order = false;
-    private boolean group = false;
-    private boolean have = false;
-    private Map<String,Column> groupContent ;
-    private Map<String,Column> orderContent ;
-    private Map<String,Where> havingConditions ;
+    private List<String> content = new ArrayList<>();//查询的名称 外联的
+    private String special = null;
+    private List<Expression> conditions = new ArrayList<>();
+    private List<String> joins = null;
+    private String groupContent = null;
+    private String orderContent = null;
+    private String havingConditions = null;
+
+    public Select sc(String column) {
+        this.content.add(column);
+        return this;
+    }
+
+    public Select sp(String sp) {
+        this.special = sp;
+        return this;
+    }
+
+    public Select join(String join) {
+        this.joins.add(join);
+        return this;
+    }
+
+    public Select group(String group){
+        this.groupContent = group;
+        return this;
+    }
+
+    public Select order(String order){
+        this.orderContent = order;
+        return this;
+    }
+
+    public Select having(String having){
+        this.havingConditions = having;
+        return this;
+    }
+
+    //接下来要处理查询条件
+    public Select op(String sql,String operation, List values){
+        return this;
+    }
 
 
 }
